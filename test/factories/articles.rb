ffa793f31c1af7dc5119fefb2d9375810ff216d5
Braulio Carreno <bcarreno@yahoo.com>
2012-04-12 19:26:16 -0400

    Initial commit

---
+++ test/factories/articles.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :article do
+    title "MyString"
+    keywords "MyString"
+    body "MyText"
+  end
+end
