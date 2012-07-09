a21eae66254c8c87fd4b8093cb46b264f61c01d2
Braulio Carreno <bcarreno@yahoo.com>
2012-07-09 13:49:42 -0400

    Improve validation email

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ActiveRecord::Base
   attr_accessible :body, :email, :name
-  validates :email, :body, :presence => true
-  validates :email, :format => { :with => /\s*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\s*/}#, :message => "Only letters allowed" }
+  validates :email, :format => {:with => /\A\s*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\s*\Z/}
 end
