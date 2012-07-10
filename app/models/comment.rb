5cb3005208c5b9a479c91786bbce61bef7bed3ac
Braulio Carreno <bcarreno@yahoo.com>
2012-07-10 14:06:30 -0400

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   attr_accessible :article_id, :body, :email, :name, :created_at
   belongs_to :article
   validates :name, :body, :presence => true
+  validates :email, :format => {:with => /\A\s*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\s*\Z/}, :unless => "email.blank?"
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   attr_accessible :body, :email, :name
+  validates :body, :presence => true
   validates :email, :format => {:with => /\A\s*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\s*\Z/}
 end
