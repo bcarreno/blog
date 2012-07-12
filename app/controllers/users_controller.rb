3502905ca36830ba1c893a537a7c4431b0ab4c05
Braulio Carreno <bcarreno@yahoo.com>
2012-07-11 21:47:16 -0400

    Don't login automatically after creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Signed up!"
+      #session[:user_id] = @user.id
+      redirect_to root_url, :notice => "User created."
     else
       render "new"
     end
