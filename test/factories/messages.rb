fda72769fc57cdeefc7e12f67ad0ae427caf9ff2
Braulio Carreno <bcarreno@yahoo.com>
2014-09-08 21:40:48 -0400

    Remove factories folder

--- test/factories/messages.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :message do
-    name "MyString"
-    email "MyString"
-    body "MyText"
-  end
-end
--- test/factories/pages.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :page do
-    title "MyString"
-    markup "MyText"
-  end
-end
