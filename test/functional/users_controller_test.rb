ba6ce894335a5cdf374429b96a31fc44682381e5
Braulio Carreno <bcarreno@yahoo.com>
2015-01-24 13:51:29 -0500

    Add user tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,9 +1,64 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+
+  def setup
+    @user_attributes = {username: 'test', email: 'test@test.com', password: 'test', name: 'John Test'}
+    @user_attributes[:password_confirmation] = @user_attributes[:password]
+  end
+
+  test "get new" do
+    get :new
+    assert_redirected_to login_path
+    assert_nil assigns(:user)
+  end
+
+  test "get new logged in as regular user" do
+    login_user(:regular)
+    get :new
+    assert_redirected_to root_path
+    assert_nil assigns(:user)
+  end
+
+  test "get new logged in as admin" do
+    login_user(:admin)
     get :new
     assert_response :success
+    assert assigns(:user)
+  end
+
+  test "post create" do
+    assert_difference('User.count', 0) do
+      post :create, user: @user_attributes
+    end
+    assert_redirected_to login_path
+    assert_nil assigns(:user)
+  end
+
+  test "post create logged in as regular user" do
+    login_user(:regular)
+    assert_difference('User.count', 0) do
+      post :create, user: @user_attributes
+    end
+    assert_redirected_to root_path
+    assert_nil assigns(:user)
+  end
+
+  test "post create logged in as admin" do
+    login_user(:admin)
+    assert_difference('User.count') do
+      post :create, user: @user_attributes
+    end
+    assert_redirected_to root_path
+    assert assigns(:user)
+    refute assigns(:user).admin
+  end
+
+  test "post create should not allow to create an admin user" do
+    login_user(:admin)
+    assert_raises(ActiveModel::MassAssignmentSecurity::Error) do
+      post :create, user: @user_attributes.merge(admin: true)
+    end
   end
 
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user_attributes = {username: 'test', email: 'test@test.com', password: 'test', name: 'John Test'}
+    @user_attributes[:password_confirmation] = @user_attributes[:password]
+  end
+
+  test 'required attributes' do
+    user = User.new
+    refute user.valid?
+    [:username, :email, :name].each do |field|
+      assert_equal "can't be blank", user.errors[field].first
+    end
+  end
+
+  test 'create new user' do
+    user = User.new(@user_attributes)
+    assert user.valid?
+    assert_difference('User.count') do
+      assert user.save
+    end
+    assert user.password_digest.strip.size >= 60
+  end
+
+  test 'do not duplicate username' do
+    user = User.new(@user_attributes.merge(username: users(:regular).username))
+    refute user.valid?
+  end
+
+  test 'confirmation matches password' do
+    #### we have to make sure an empty pwd confirmation returns '' and not nil, otherwise the test isn't enough
+    user = User.new(@user_attributes.merge(password_confirmation: 'different'))
+    refute user.valid?
+    assert_equal "doesn't match confirmation", user.errors[:password].first
+  end
+
+  test 'digests should be different for the same password' do
+    user1 = User.create!(@user_attributes)
+    user2 = User.create!(@user_attributes.merge(username: 'test2'))
+    refute_equal user1.password_digest.to_s, user2.password_digest.to_s
+  end
+
 end
