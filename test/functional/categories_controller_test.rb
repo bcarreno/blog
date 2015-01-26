3f86c59b553783af6fc2050a8460c55c6bebcb9e
Braulio Carreno <bcarreno@yahoo.com>
2015-01-26 18:44:31 -0500

    Categories functional tests

--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -2,48 +2,153 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
   setup do
-    @category = categories(:one)
+    @category = categories(:sports)
   end
 
-  test "should get index" do
+  test "get index" do
+    get :index
+    assert_redirected_to login_path
+    assert_nil assigns(:categories)
+  end
+
+  test "get index logged in as regular user" do
+    login_user(:regular)
+    get :index
+    assert_redirected_to root_path
+    assert_nil assigns(:categories)
+  end
+
+  test "get index logged in as admin" do
+    login_user(:admin)
     get :index
     assert_response :success
-    assert_not_nil assigns(:categories)
+    assert assigns(:categories)
   end
 
-  test "should get new" do
+  test "get new" do
+    get :new
+    assert_redirected_to login_path
+    assert_nil assigns(:category)
+  end
+
+  test "get new logged in as regular user" do
+    login_user(:regular)
+    get :new
+    assert_redirected_to root_path
+    assert_nil assigns(:category)
+  end
+
+  test "get new logged in as admin" do
+    login_user(:admin)
     get :new
     assert_response :success
+    assert assigns(:category)
   end
 
-  test "should create category" do
+  test "post create" do
+    assert_difference('Category.count', 0) do
+      post :create, category: { description: @category.description, name: @category.name }
+    end
+    assert_redirected_to login_path
+    assert_nil assigns(:category)
+  end
+
+  test "post create logged in as regular user" do
+    login_user(:regular)
+    assert_difference('Category.count', 0) do
+      post :create, category: { description: @category.description, name: @category.name }
+    end
+    assert_redirected_to root_path
+    assert_nil assigns(:category)
+  end
+
+  test "post create logged in as admin" do
+    login_user(:admin)
     assert_difference('Category.count') do
       post :create, category: { description: @category.description, name: @category.name }
     end
-
     assert_redirected_to category_path(assigns(:category))
   end
 
-  test "should show category" do
+  test "get show" do
+    get :show, id: @category
+    assert_redirected_to login_path
+    assert_nil assigns(:category)
+  end
+
+  test "get show logged in as regular user" do
+    login_user(:regular)
+    get :show, id: @category
+    assert_redirected_to root_path
+    assert_nil assigns(:category)
+  end
+
+  test "get show logged in as admin" do
+    login_user(:admin)
     get :show, id: @category
     assert_response :success
+    assert assigns(:category)
   end
 
-  test "should get edit" do
+  test "get edit" do
+    get :edit, id: @category
+    assert_redirected_to login_path
+    assert_nil assigns(:category)
+  end
+
+  test "get edit logged in as regular user" do
+    login_user(:regular)
+    get :edit, id: @category
+    assert_redirected_to root_path
+    assert_nil assigns(:category)
+  end
+
+  test "get edit logged in as admin" do
+    login_user(:admin)
     get :edit, id: @category
     assert_response :success
+    assert assigns(:category)
   end
 
-  test "should update category" do
+  test "put update" do
+    put :update, id: @category, category: { description: @category.description, name: @category.name }
+    assert_redirected_to login_path
+    assert_nil assigns(:category)
+  end
+
+  test "put update logged in as regular user" do
+    login_user(:regular)
+    put :update, id: @category, category: { description: @category.description, name: @category.name }
+    assert_redirected_to root_path
+    assert_nil assigns(:category)
+  end
+
+  test "put update logged in as admin" do
+    login_user(:admin)
     put :update, id: @category, category: { description: @category.description, name: @category.name }
     assert_redirected_to category_path(assigns(:category))
   end
 
-  test "should destroy category" do
+  test "delete destroy" do
+    assert_difference('Category.count', 0) do
+      delete :destroy, id: @category
+    end
+    assert_redirected_to login_path
+  end
+
+  test "delete destroy logged in as regular user" do
+    login_user(:regular)
+    assert_difference('Category.count', 0) do
+      delete :destroy, id: @category
+    end
+    assert_redirected_to root_path
+  end
+
+  test "delete destroy logged in as admin" do
+    login_user(:admin)
     assert_difference('Category.count', -1) do
       delete :destroy, id: @category
     end
-
     assert_redirected_to categories_path
   end
 end
