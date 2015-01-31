9cf0a4d3bc9c4ddc827c2a459955a027b98dbda5
Braulio Carreno <bcarreno@yahoo.com>
2015-01-31 14:27:37 -0500

    Tests on sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :email, :name, :username
+  validates_presence_of :password, on: :create
   validates_uniqueness_of :username
 end
--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,9 +1,45 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
+
+  def setup
+    @user = users(:regular)
+  end
+
+  test "get new" do
     get :new
     assert_response :success
+  end
+
+  # what if already logged in?
+  test "get new as a logged in user" do
+    login_user(:regular)
+    get :new
+    assert_response :success
+  end
+
+  # what if already logged in? (make new test)
+  test "login with correct credentials" do
+    post :create, username: @user.username, password: 'secret'
+    assert_equal @user.id, session[:user_id]
+  end
+
+  test "login with bad credentials" do
+    post :create, username: @user.username, password: 'bad secret'
+    assert_nil session[:user_id]
+  end
+
+  test "logout" do
+    login_user(:regular)
+    get :destroy
+    assert_nil session[:user_id]
+    assert_redirected_to root_path
+  end
+
+  test "logout without being logged in" do
+    get :destroy
+    assert_nil session[:user_id]
+    assert_redirected_to root_path
   end
 
 end
