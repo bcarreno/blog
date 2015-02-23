c9657b23a5695be64dbf10da46bcdd452fe08543
Braulio Carreno <bcarreno@yahoo.com>
2015-02-23 15:49:03 -0500

    tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :name
   has_and_belongs_to_many :articles
+  validates_presence_of :name
 end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'required attributes' do
+    category = Category.new
+    refute category.valid?
+    [:name].each do |field|
+      assert_equal "can't be blank", category.errors[field].first
+    end
+  end
+
+  test 'create new category' do
+    category = Category.new(name: 'Horror', description: 'Terror & Horror')
+    assert category.valid?
+    assert_difference('Category.count') do
+      assert category.save
+    end
+  end
+
 end
