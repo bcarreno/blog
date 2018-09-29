a8a091a28fe6211aaf26240c90f69b5b04598683
Braulio Carreno <bcarreno@yahoo.com>
2018-09-29 17:18:09 -0400

    Workaround not needed anymore
    Also it generates an error.

--- config/initializers/haml.rb
+++
@@ -1,7 +0,0 @@
-# Fix excess spaces while rendering multiline string
-# Should be fixed in HAML 5
-# https://github.com/haml/haml/issues/869
-require 'haml/template'
-
-Haml::Template.options[:ugly] = true
-
