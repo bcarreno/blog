41a9c78dc008d1c3765363672d39f3dbead93338
Braulio Carreno <bcarreno@yahoo.com>
2018-10-09 22:52:45 -0400

    More deprecation warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Blog4::Application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
